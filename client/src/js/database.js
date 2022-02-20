df5db668192c9b042307ce8c227f3cf7bc295ecf
Juwonoh41 <edmaster41@gmail.com>
2022-02-20 15:28:11 -0800

    get

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,7 +15,7 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   const db = await openDB('jate, 1')
-  const text = db.transaction('jate', 'readwrite')
+  const text = db.transaction('jate', 'readAndWrite')
   const storage = text.objectStore('jate')
   const req = storage.put({
     id: 1, value: content
@@ -26,6 +26,17 @@ export const putDb = async (content) => {
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const db = await openDB('jate', 1)
+  const text = db.transaction('jate', 'read')
+  const storage = text.objectStore('jate')
+  const req = storage.get(1)
+  const res = await req
+  //conditional ? acts as first if, : acts as else
+  res 
+    ? console.log('Success', res.value)
+    : console.log('Not Found')
+  return res?.value
+};
 
 initdb();
