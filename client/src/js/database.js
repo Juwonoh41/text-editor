cf715a56b251c1c2b85044f72cc05c6e3350905d
Juwonoh41 <edmaster41@gmail.com>
2022-02-20 15:36:11 -0800

    fixed put route

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,7 +14,7 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  const db = await openDB('jate, 1')
+  const db = await openDB('jate', 1)
   const text = db.transaction('jate', 'readAndWrite')
   const storage = text.objectStore('jate')
   const req = storage.put({
